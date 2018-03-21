52708d70ad1906b24125b2d6503e99b66b24e335
Oliwia <oliwia.m.96@wp.pl>
2018-03-21 19:30:43 +0100

    Swap elements

--- public_html/form_check.js
+++ public_html/form_check.js
@@ -184,7 +184,7 @@ function hideElement(e) {
 }
 
 // i is a row iterator,
- // e is a row
+// e is a row
 function alterRows(i, e) {
     if (e) {
         if (i % 2 == 1) {
@@ -196,4 +196,25 @@ function alterRows(i, e) {
         }
         alterRows(++i, e);
     }
+}
+
+function nextNode(e) {
+    while (e && e.nodeType != 1) {
+        e = e.nextSibling;
+    }
+    return e;
+}
+function prevNode(e) {
+    while (e && e.nodeType != 1) {
+        e = e.previousSibling;
+    }
+    return e;
+}
+function swapRows(b) {
+    var tab = prevNode(b.previousSibling);
+    var tBody = nextNode(tab.firstChild);
+    var lastNode = prevNode(tBody.lastChild);
+    tBody.removeChild(lastNode);
+    var firstNode = nextNode(tBody.firstChild);
+    tBody.insertBefore(lastNode, firstNode);
 }
